4df442b910ff2aa523103bac89fabfeac31cae9f
Amir <amirpitaev01@gmail.com>
2025-07-11 17:15:07 +0300

    add lenta

--- app/Models/Friendship.php
+++ app/Models/Friendship.php
@@ -5,5 +5,11 @@ use Illuminate\Database\Eloquent\Model;
 class Friendship extends Model
 {
     public $table = 'friendships';
+
+    protected $fillable = [
+        'user_id',
+        'friend_id',
+    ];
+
     protected $guarded = [];
 }
